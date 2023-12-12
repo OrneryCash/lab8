4bbfb6185f33736f8eb2f6cd15208cb15301ae83
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-12-12 15:39:23 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,41 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string character;
+  int age;
+  cout << "Enter your age: ";
+  cin >> age;
+  if (age <= 25) {
+    int height;
+    cout << "Enter your height: ";
+    cin >> height;
+    if (height < 100) {
+      character = "Chopper";
+    } else if (height < 180) {
+      character = "Usopp";
+    } else {
+      int bounty;
+      cout << "Enter your bounty: ";
+      cin >> bounty;
+      if (bounty > 1.1e9) {
+        character = "Zoro";
+      } else {
+        character = "Sanji";
+      }
+    }
+  } else if (age <= 60) {
+    int bounty;
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+    if (bounty > 5e8) {
+      character = "Jinbe";
+    } else {
+      character = "Franky";
+    }
+  } else {
+    character = "Brook";
+  }
+  cout << "Your character = " << character;
+}
